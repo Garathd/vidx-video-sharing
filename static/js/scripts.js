47aa89946de8c58f66a7b58383895e8acd98437e
Garath Davis <garath.davis.gti@gmail.com>
2019-02-26 17:23:24 +0000

    Fixed mobile menu expand issue

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,6 +5,15 @@ $(document).ready(function() {
     $(".button-collapse").sideNav();
 });
 
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+});
+
 var btn = $('#the-top');
 
 $(window).scroll(function() {
